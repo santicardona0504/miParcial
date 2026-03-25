21e159e85151a8eae12c78a78728dd74a567b245
santi <s.cardona7@utp.edu.co>
2026-03-24 19:49:18 -0500

    agregar requirements.txt y actualizar main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Hola mundo')
+from fastapi import FastAPI 
+from dotenv import load_dotenv 
+app = FastAPI() 
+load_dotenv() 
+@app.get("/llm/{prompt}") 
+async def read_root(prompt): 
+from google import genai 
+# The client gets the API key from the environment variable `GEMINI_API_KEY`. client = genai.Client() 
+response = client.models.generate_content( 
+model="gemini-3-flash-preview", contents=prompt 
+) 
+print(response.text) 
+return {"Respuesta": response.text} 
